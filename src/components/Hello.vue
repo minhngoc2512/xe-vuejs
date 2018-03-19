44e45fa45db5860592f2428a65c836e6a5af8785
minhngoc2512 <minhngoc2512@yahoo.com>
2018-03-20 01:19:42 +0700

    update

---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <button v-on:click="getData()">{{demo}}</button>
+  </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            demo: 'demo'
+        }
+    },
+    methods:{
+        getData(){
+            window.axios.get('https://xe.vatgia.com/api/rewrites/xe-hoi-mazda').then(response =>{
+                console.log(response.data);
+            });
+            console.log(this.$route);
+       
+        }
+    }
+}
+</script>
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <hello></hello>
+  </div>
+</template>
+<script>
+import Hello from "../../components/Hello.vue"
+export default {
+    components:{
+        hello: Hello
+    }
+  
+}
+</script>
+
